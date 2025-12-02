0e02efe1ecc90a25215b3a2dacae14ff85be9fbd
Raul Ferreira da Rocha <raulferreiradarocha1@hotmail.com>
2025-12-01 21:32:07 -0300

    fix deploy

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,7 +4,12 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
-    await this.$connect();
+    try {
+        await this.$connect();
+    } catch (e) {
+        console.error("Erro ao conectar o Prisma na inicialização:", e);
+        throw e;
+    }
   }
   async onModuleDestroy() {
     await this.$disconnect();
